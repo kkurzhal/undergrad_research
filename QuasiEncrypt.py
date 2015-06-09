8e916df202731718e2c98801dbc13e301d2c9cde
Kyle Kurzhal <kkurzhal@ipwatch.com>
2015-06-09 00:11:32 -0500

    Added some functions that will be needed to the QuasiEncrypt.py and began writing encoding functionality (needs tests).  Added the ability to pass in the first row of the latin square for testing.  Wrote a more comprehensive test for the latin square construction since noticing a discrepency in an example.

--- QuasiEncrypt.py
+++ QuasiEncrypt.py
@@ -8,9 +8,10 @@ class QuasiAON(object):
     to another encryption method to increase security.
     """
     def __init__(self):
+        #This must always be a prime number to get a true latin square.
         self.last_num = 7
 
-    def construct_latin_square(self):
+    def construct_latin_square(self, first_row = []):
         """
         Construct the matrix needed to encode and decode the pseudomessage.
         """
@@ -18,8 +19,12 @@ class QuasiAON(object):
         
 
         #Get the first row of integers in a random order.
-        first_row = range(1, self.last_num)
-        random.shuffle(first_row)
+
+        #If the first row is passed to the function then use it, otherwise make
+        #a random first row.
+        if len(first_row) == 0:
+            first_row = range(1, self.last_num)
+            random.shuffle(first_row)
 
         #Add the first row to the matrix
         matrix.append(first_row)
@@ -41,6 +46,60 @@ class QuasiAON(object):
             matrix.append(each_row)
 
         return matrix
+
+    def encode_message(self, number_message, latin_square, leader):
+        """
+        Encode the message using the AON methodology.
+        """
+        #Initialize the encoded message and previous number variables.
+        previous_number = 0
+        encoded_message = ''
+
+        for each_number in number_message:
+            number_as_integer = int(each_number)
+            
+            #Start the encoded message by mapping the first number in the number
+            #message.
+            if each_number == number_message[0]:
+                print 'initial number'
+                previous_number = latin_square[leader - 1][number_as_integer - 1]
+
+            #If it isn't the start, then use the previously mapped quasigroup
+            #instead of the leader.
+            else:
+                print 'following number'
+                previous_number = latin_square[previous_number - 1][number_as_integer - 1]
+
+            encoded_message = str(previous_number)
+
+        return encoded_message
+
+    def decode_message(self, number_message, matrix, leader):
+        """
+        Decode the message using the AON methodology.
+        """
+        pass
+
+    def package_message(self, encoded_message, matrix, leader):
+        """
+        Prepare the message for sending.
+        """
+        pass
+
+    def unpackage_message(self, packet):
+        """
+        Unpack the message after it is received.
+        """
+        pass
+
+    def convert_message_to_number_string(self, message):
+        """
+        Convert the binary representing the characters into a string of
+        numbers  for the encoding.
+        """
+        #Get the value of each character and format it in terms of bits.
+        number_list = [str(ord(character)) for character in message]
+        return ''.join(number_list)
 
     def print_matrix(self, matrix):
         for each_row in matrix:
--- test_QuasiEncrypt.py
+++ test_QuasiEncrypt.py
@@ -1,4 +1,5 @@
 import unittest
+from itertools import permutations
 from QuasiEncrypt import QuasiAON
 
 class TestQuasiAON(unittest.TestCase):
@@ -9,14 +10,51 @@ class TestQuasiAON(unittest.TestCase):
     def setUp(self):
         self.aon_instance = QuasiAON()
     
-    def test_construct_matrix(self):
+    def test_random_construct_latin_square(self):
         """
-        Test construct_matrix() to make sure that a true latin square is
-        created.
+        Test construct_latin_square() to make sure that a true latin
+        square is created by running the construct function a specified
+        number of times.
         """
         #Test the matrix construction as many times as is specified.
         for each_test in range(50):
-            matrix = self.aon_instance.construct_matrix()
+            matrix = self.aon_instance.construct_latin_square()
+
+            #Check to make sure that the resulting matrix is square.
+            self.assertTrue(len(matrix[0]) == len(matrix))
+
+            #Check the rows to make sure that no duplicates values exist.
+            for each_row in matrix:
+
+                #Create a unique set of values.
+                unique_list = set(each_row)
+
+                #Check that the length of the unique set matches the length of
+                #the original row.
+                self.assertTrue(len(unique_list) == len(each_row))
+
+            #Check the columns to make sure that no duplicates exist.
+            for each_column in range(len(matrix)):
+
+                #Get the column of values.
+                column = map(lambda row: row[each_column], matrix)
+
+                #Get the unique set of values in the column.
+                unique_column = set(column)
+
+                #Check that the length of the unique set matches the length of
+                #the original column.                
+                self.assertTrue(len(unique_column) == len(column))
+
+    def test_all_latin_squares_for_set_dimension(self):
+        """
+        Test all possible combinations of a latin square based on the
+        specified dimensions.
+        """
+        #Test the matrix construction for all combinations of a range.
+        number_range = range(1, self.aon_instance.last_num)
+        for each_test in permutations(number_range):
+            matrix = self.aon_instance.construct_latin_square(each_test)
 
             #Check to make sure that the resulting matrix is square.
             self.assertTrue(len(matrix[0]) == len(matrix))
@@ -46,5 +84,3 @@ class TestQuasiAON(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-        
